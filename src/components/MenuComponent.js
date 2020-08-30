15dbea7a2a35b38716769ac8c8ad1abec91ac8a6
coderprasukj <prasukjain0047@gmail.com>
2020-08-30 13:53:32 +0530

    Fetch from Server

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
-function RenderMenuItem ({dish, onClick}) {
+function RenderMenuItem({ dish, onClick }) {
     return (
         <Card>
             <Link to={`/menu/${dish.id}`} >
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
                 </CardImgOverlay>
@@ -25,16 +26,16 @@ const Menu = props => {
         )
     });
 
-    if(props.dishes.isLoading) {
-         return (
-                <div className="container">
-                    <div className="row">
-                        <Loading />
-                    </div>
+    if (props.dishes.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
                 </div>
-            );
+            </div>
+        );
     }
-    else if(props.errMess) {
+    else if (props.dishes.errMess) {
         return (
             <div className="container">
                 <div className="row">
@@ -54,7 +55,7 @@ const Menu = props => {
                     <div className="col-12">
                         <h3>Menu</h3>
                         <hr />
-                    </div>                
+                    </div>
                 </div>
                 <div className="row">
                     {menu}
